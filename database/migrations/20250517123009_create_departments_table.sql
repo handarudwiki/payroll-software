b42fb78dcd3ca20c759e04b7973861f327253a65
handarudwiki <handarudwiki04@gmail.com>
2025-05-17 19:31:47 +0700

    feat: create departments table

---
+++ database/migrations/20250517123009_create_departments_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE departments(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE departments;
+-- +goose StatementEnd
