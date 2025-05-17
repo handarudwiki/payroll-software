bf9c6bc706719bf9a36f2f38438c72265672a501
handarudwiki <handarudwiki04@gmail.com>
2025-05-17 23:10:13 +0700

    feat: create salary components table

---
+++ database/migrations/20250517160816_create_salary_components_table.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TYPE salary_component_type AS ENUM ('allowance', 'recurring');
+
+CREATE TABLE salary_components (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    type salary_component_type NOT NULL DEFAULT 'allowance',
+    is_recurring BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE salary_components;
+DROP TYPE salary_component_type;
+-- +goose StatementEnd
